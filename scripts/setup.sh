d83221598faa601a91a9abfb0866236f6c33c069
Mudara-Fernando <mudara.madusanka@gmail.com>
2024-06-28 07:07:37 +0530

    new

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,13 @@
 
 sudo apt update
 
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo usermod -aG docker ${USER}
+
 sudo apt install -y nginx certbot python3-certbot-nginx
 sudo apt install -y certbot python3-certbot-nginx
 
